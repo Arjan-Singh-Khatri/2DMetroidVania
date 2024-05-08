9a599c01666f3bda89eaf38d8a8f6f0f11399f4e
Arjan-Singh-Khatri <arzankhatri99@gmail.com>
2024-05-08 19:09:33 +0545

    Design
    Main Menu

---
+++ Assets/Main game folder/script/MainMenu/BatDoTweenUI.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class BatDoTweenUI : MonoBehaviour
+{
+    private RectTransform rectTransform;
+    
+    void Start(){
+        rectTransform = GetComponent<RectTransform>();
+        
+    }
+}
--- Assets/Main game folder/script/MainMenu/SlimeUIDoTween.cs
+++ Assets/Main game folder/script/MainMenu/SlimeUIDoTween.cs
@@ -5,15 +5,20 @@ using DG.Tweening;
 
 public class SlimeUIDoTween : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private RectTransform rectTransform;
+    [SerializeField] Vector3 _endPoint;
+    [SerializeField] float _jumpPower;
+    [SerializeField] int _jumpNumber;
+    [SerializeField] float _jumpDuration;
 
+    private void Start(){
+        rectTransform = GetComponent<RectTransform>();
+        SlimeJumpBackAndForth();
+    }
     // Update is called once per frame
-    void Update()
-    {
-        
+    void SlimeJumpBackAndForth(){
+        rectTransform.DOJumpAnchorPos(_endPoint, _jumpPower,
+            _jumpNumber, _jumpDuration).SetLoops(-1, LoopType.Yoyo);
+
     }
 }
